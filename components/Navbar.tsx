3bd11314538a41aacbdfef955d13a468f69b3f11
Janesh Timilsena <111886149+janesh-design@users.noreply.github.com>
2023-07-01 21:19:20 +0545

    Complete project commit

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,19 +2,19 @@ import Image from "next/image";
 import Link from "next/link";
 
 import { NavLinks } from "@/constant";
-// import { getCurrentUser } from "@/lib/session";
+import { getCurrentUser } from "@/lib/session";
 
 import AuthProviders from "./AuthProviders";
-// import Button from "./Button";
-// import ProfileMenu from "./ProfileMenu";
+import Button from "./Button";
+import ProfileMenu from "./ProfileMenu";
 
 const Navbar = async () => {
-  // const session = await getCurrentUser()
-  const session = {};
+  const session = await getCurrentUser()
 
   return (
     <nav className='flexBetween navbar'>
       <div className='flex-1 flexStart gap-10'>
+
         <Link href='/'>
           <Image
             src='/logo.svg'
@@ -23,6 +23,7 @@ const Navbar = async () => {
             alt='logo'
           />
         </Link>  
+
         <ul className='xl:flex hidden text-small gap-7'>
           {NavLinks.map((link) => (
             <Link href={link.href} key={link.text}>
@@ -33,12 +34,12 @@ const Navbar = async () => {
       </div>
 
       <div className='flexCenter gap-4'>
-        {session ? (
+        {session?.user ? (
           <>
-            {/* <ProfileMenu session={session} /> */}
+            <ProfileMenu session={session} />
 
             <Link href="/create-project">
-              {/* <Button title='Share work' /> */}
+              <Button title='Share work' />
             </Link>
           </>
         ) : (
